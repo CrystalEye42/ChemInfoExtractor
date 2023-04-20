03e309720e14ef28ea54d31f271928bceee8fe47
Alex Wang <alexwang7470@gmail.com>
2023-04-20 14:53:33 -0400

    changed to effdet for compatibility

--- backend/model.py
+++ backend/model.py
@@ -23,10 +23,7 @@ class Models:
         self.device = 'cuda' if torch.cuda.is_available() else 'cpu'
         print('Using', self.device)
         print('Loading layout')
-        self.layout = lp.Detectron2LayoutModel('lp://PubLayNet/mask_rcnn_X_101_32x8d_FPN_3x/config', 
-                extra_config=["MODEL.ROI_HEADS.SCORE_THRESH_TEST", 0.5], 
-                label_map={0: "Text", 1: "Title", 2: "List", 3:"Table", 4:"Figure"}, 
-                device=self.device)
+        self.layout = lp.AutoLayoutModel("lp://efficientdet/PubLayNet/tf_efficientdet_d1", device=self.device)
 
         print('Loading reaction')
         self.reaction = ReactionModel()

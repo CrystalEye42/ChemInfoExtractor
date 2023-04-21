249e828175b4277f583ca1d6de79024febea359e
Guy Zylberberg <guyzyl@gmail.com>
2023-04-21 13:24:40 -0400

    Detect when changes are made in Ketcher editor

--- frontend/src/components/KetcherDisplay.js
+++ frontend/src/components/KetcherDisplay.js
@@ -8,17 +8,23 @@ export class KetcherDisplay extends React.Component {
         super(props);
         this.state = {
             scroll: true,
+            smiles: null,
         };
         this.refFrame = React.createRef();
         this.display = this.display.bind(this);
         this.startPolling = this.startPolling.bind(this);
+        this.checkSmiles = this.checkSmiles.bind(this);
+
+        this.checkSmilesLoop = setInterval(this.checkSmiles, 5000);
     }
 
     startPolling() {
         if (this.refFrame.current && this.refFrame.current.contentWindow) {
-            this.display();
+            this.display()
             return;
         }
+
+        this.setState({smiles: null})
         setTimeout(this.startPolling, 100);
      }
 
@@ -36,7 +42,24 @@ export class KetcherDisplay extends React.Component {
                         document.getElementById("results").scrollTo(0,0);
                     }
                     this.setState({scroll: false});
+                    ketcher.getSmiles().then(smiles => {
+                        this.setState({smiles: smiles});
+                    });
                 }));
+        }
+    }
+
+    async checkSmiles() {
+        const { current } = this.refFrame;
+        if (!current || !current.contentWindow || !this.state.smiles) return;
+
+        const { ketcher } = current.contentWindow;
+        if (!ketcher) return;
+
+        const newSmiles = await ketcher.getSmiles();
+        if (newSmiles !== this.state.smiles) {
+            // TODO: Do something here!
+            this.setState({smiles: newSmiles});
         }
     }
 
@@ -51,10 +74,10 @@ export class KetcherDisplay extends React.Component {
             <div>
                 {!this.props.molblock && <p>No Mol block predicted</p>}
                 <div id="wrap"></div>
-                <iframe 
+                <iframe
                     id="frame"
-                    title="myiframe" 
-                    ref={this.refFrame} 
+                    title="myiframe"
+                    ref={this.refFrame}
                     onLoad={() => {
                         this.setState({scroll: true});
                         this.startPolling();}}

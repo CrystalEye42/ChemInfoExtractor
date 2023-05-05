2f18ad012f1f039e110ead98b1fb022e779eb281
Guy Zylberberg <guyzyl@gmail.com>
2023-05-05 16:03:59 -0400

    Create "createBoxes" and make it more flexible

--- frontend/src/components/FigureImageDisplay.js
+++ frontend/src/components/FigureImageDisplay.js
@@ -2,10 +2,11 @@ import React from "react";
 import PropTypes from 'prop-types';
 import { View, StyleSheet } from "react-native";
 
-const borderWidth = 3;
+
 // component for drawing bounding boxes on figure images
 export class FigureImageDisplay extends React.Component {
     TEXT_OFFSET = 20;
+
     constructor(props) {
         super(props);
         this.state = {
@@ -20,55 +21,57 @@ export class FigureImageDisplay extends React.Component {
         this.setState({height:offsetHeight + this.TEXT_OFFSET, width:offsetWidth});
     }
 
-    drawBox(bbox) {
+    createBoxes(boxes) {
+        return boxes.map((box, i) => {
+            const [x1, y1, x2, y2] = box["bbox"];
+            const label = isNaN(box["label"]) ? box["label"] : box["label"].toFixed(4);
+            const borderWidth = box["style"]["borderWidth"] || 0;
+
+            return (
+                <div key={"det"+i}
+                    onClick={(e) => {
+                        this.props.callback(i);
+                    }}>
+                    <View style={[
+                        box["style"],
+                        {
+                            top: y1 * this.state.height - borderWidth - this.TEXT_OFFSET,
+                            height: (y2 - y1) * this.state.height + 2 * borderWidth + this.TEXT_OFFSET,
+                            left: x1 * this.state.width - borderWidth,
+                            width: (x2 - x1) * this.state.width + 2 * borderWidth,
+                        }
+                    ]}>{label}</View>
+                </div>
+            );
+        });
+    }
+
+    drawBoxes(bbox) {
         if (!bbox) {
             return (
                 <img src={`data:image/jpeg;base64,${this.props.details["figure"]}`} id="mainimg" alt="main"/>
             );
         }
-        const [x1, y1, x2, y2] = bbox;
-        const otherBoxes = this.props.details["subfigures"].map((info, i) => {
-            const [x1, y1, x2, y2] = info[2];
-            const score = info[3].toFixed(4);
-            return (
-            <div key={"det"+i}
-                onClick={(e) => {
-                    this.props.callback(i);
-                }}>
-                <View style={[
-                    styles.rectangleShaded,
-                    {
-                        top: y1*this.state.height-this.TEXT_OFFSET,
-                        height: (y2-y1)*this.state.height+this.TEXT_OFFSET,
-                        left: x1*this.state.width,
-                        width: (x2-x1)*this.state.width,
-                    }
-                ]}>{score}</View>
-            </div>);
-        });
+
+        const subfigures = this.props.details["subfigures"].map((subfigure) => ({bbox: subfigure[2], label: subfigure[3], style: styles.rectangleShaded}));
+        subfigures.push({bbox: bbox, style: styles.rectangle});
+        const boxes = this.createBoxes(subfigures);
+
         return (
             <View style={styles.imageContainer}>
                 <img src={`data:image/jpeg;base64,${this.props.details["figure"]}`} id="mainimg" onLoad={this.onImgLoad} alt="main"/>
-                {otherBoxes}
-                <View style={[
-                    styles.rectangle,
-                    {
-                        top: y1*this.state.height-borderWidth-this.TEXT_OFFSET,
-                        height: (y2-y1)*this.state.height+2*borderWidth+this.TEXT_OFFSET,
-                        left: x1*this.state.width-borderWidth,
-                        width: (x2-x1)*this.state.width+2*borderWidth,
-                    }
-                ]}></View>
+                {boxes}
             </View>
         );
     }
 
     render() {
-        const subfigure =  this.props.value >=0 ? this.props.details["subfigures"][this.props.value]:null;
+        console.log(this.props) // TODO: Remove
+        const subfigure =  this.props.value >=0 ? this.props.details["subfigures"][this.props.value] : null;
         const bbox = subfigure ? subfigure[2] : null;
         return (
         <div id="imagedisp">
-            <View style={styles.container}>{this.drawBox(bbox)}</View>
+            <View style={styles.container}>{this.drawBoxes(bbox)}</View>
         </div>);
     }
 }
@@ -90,7 +93,7 @@ const styles = StyleSheet.create({
         alignSelf: "center"
     },
     rectangle: {
-        borderWidth: borderWidth,
+        borderWidth: 3,
         borderColor: "red",
         borderStyle: "dashed",
         position: "absolute"

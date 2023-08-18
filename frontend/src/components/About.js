8a330c3f83c6b4d0b2837f43fecb7ca361d93df1
wang7776 <wang7776@mit.edu>
2023-08-18 16:45:24 -0400

    initial about page

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -1,4 +1,5 @@
 import React from "react";
+import './About.css'; 
 
 export class About extends React.Component {
   constructor(props) {
@@ -6,8 +7,20 @@ export class About extends React.Component {
   }
 
   render() {
-    return (<div>
-        <>Hello World</>
+    return (<div id="about-container">
+        <br></br>
+        <h1>OpenChemIE</h1>
+        <h3>Information Extraction Toolkit for Chemistry Literature</h3>
+        <br></br>
+        <div id="about-paragraph"> 
+          
+          Our code for interacting with these models is open source and is available as
+          a <a href="https://github.com/CrystalEye42/OpenChemIE" target="_blank" rel="noopener noreferrer">Python package</a>. 
+          Check out our online demo for the models <a href="/molscribe">MolScribe</a>
+          , <a href="/rxnscribe">RxnScribe</a>, <a href="/chemrxnextractor">ChemRxnExtractor</a>, 
+          and <a href="/chemner">ChemNER</a>. Alternatively, our <a href="https://github.com/CrystalEye42/ChemInfoExtractor"
+          target="_blank" rel="noopener noreferrer">website</a> is open source and can be downloaded and deployed for personal usage. 
+        </div>
     </div>);
   }
 }

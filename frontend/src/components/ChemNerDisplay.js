fc97874a246eb64646e5d32869c9f62d3e93df3b
wang7776 <wang7776@mit.edu>
2023-09-07 15:02:31 -0400

    update for new endpoint

--- frontend/src/components/ChemNerDisplay.js
+++ frontend/src/components/ChemNerDisplay.js
@@ -5,44 +5,6 @@ import './TextRxnDisplay.css';
 export class ChemNerDisplay extends React.Component {
     constructor(props) {
         super(props);
-        this.patterns = [
-          /(?<!\w)h(?=[.,;!?]|$)/g, // for hours
-          /(?<!\w)s(?=[.,;!?]|$)/g,  // for seconds
-          /(?<!\w)°C(?=[.,;!?]|$)/g,
-          /(?<!\w)min(?=[.,;!?]|$)/g,
-          /(?<!\w)hr(?=[.,;!?]|$)/g,
-          /(?<!\w)hrs(?=[.,;!?]|$)/g
-        ];
-    }
-
-
-    cleanTokens(tokens) {
-      let new_tokens = [tokens[0]];
-      for (let i = 1; i < tokens.length; i++) {
-        // Access the element at index i
-        let changed = false;
-        for(const pattern of this.patterns) {
-          if(tokens[i].match(pattern)) {
-            new_tokens[i - 1] = tokens[i - 1] + " " + tokens[i];
-            new_tokens.push("");
-            changed = true;
-            break;
-          }
-        }
-        if(!changed) {
-          new_tokens.push(tokens[i]);
-        }
-      }
-      return new_tokens;
-    }
-
-    cleanTags(tag) {
-      for(const pattern of this.patterns) {
-        if(tag[0].match(pattern)) {
-          return [tag[0], tag[1], tag[1]];
-        }
-      }
-      return tag;
     }
 
     render() {
@@ -52,63 +14,48 @@ export class ChemNerDisplay extends React.Component {
         <div>
           {
             details.map((resultDict, pageIndex) => {
-              const labels = resultDict['predictions'];
-              const sentences = resultDict['sentences'];
-              const cleanLabeledTokens = sentences.map((sentence, i) => {
-                return [sentence, labels[i]];
-              }).filter((value) => {
-                const sentenceLabels = value[1];
-                return sentenceLabels.filter(label => label != 'O').length > 0;
-              }).map((value) => {
-                const [tokens, sentenceLabels] = value;
-                return tokens.reduce((accumulator, currToken, i, original) => {
-                  let combine = false;
-                  if (currToken[0] === '#'){
-                    currToken = currToken.slice(2);
-                    combine = true;
-                  } else if ([',', '.', ';'].includes(currToken)) {
-                    combine = true;
-                  }
-                  const label = sentenceLabels[i].slice(2);
-                  if (i > 0 && (combine || (label !== '' && label === accumulator[accumulator.length - 1][1]))) {
-                    accumulator[accumulator.length - 1][0] = accumulator[accumulator.length - 1][0] + currToken;
-                  }
-                  else {
-                    accumulator.push([currToken, label]);
-                  }
-                  return accumulator;
-                }, [])
+              const page = resultDict['molecules'];
+              const labeledText = page.filter(paragraph => {
+                return paragraph['labels'].length > 0;
+              }).map((paragraph) => {
+                const text = paragraph['text'];
+                const labeledFragments = [];
+                let currIdx = 0;
+                for (const [label, [start, end]] of paragraph['labels']) {
+                  text.slice(currIdx, start).split(' ').forEach(word=>labeledFragments.push([word, '']));
+                  labeledFragments.push([text.slice(start, end), label]);
+                  currIdx = end;
+                }
+                text.slice(currIdx, text.length).split(' ').forEach(word=>labeledFragments.push([word, '']));
+                return labeledFragments;
               });
               return (
                 <div key={pageIndex}>
-                  {cleanLabeledTokens.length !== 0 && <h3 className="page-header">Page {pageIndex + 1}</h3>}
-                  {cleanLabeledTokens.length !== 0 && <br />}
-                  {cleanLabeledTokens.map((labeledSentence, index) => (
+                  {labeledText.length !== 0 && <h3 className="page-header">Page {pageIndex + 1}</h3>}
+                  {labeledText.length !== 0 && <br />}
+                  {labeledText.map((labeledParagraph, index) => (
                     <div key={index}>
-                      {<React.Fragment>
-                        <span> {index + 1}.</span>{" "}
-                      </React.Fragment>}
-                      {labeledSentence.map(([word, label], tokenIndex) => {
+                      {labeledParagraph.map(([text, label], tokenIndex) => {
                         let className = label === '' ? "normal-word" : `${label.toLowerCase()}`; 
 
                         return (
                           <>
-                           {className==="normal-word" ? (
+                            {className==="normal-word" ? (
                               <React.Fragment key={tokenIndex}>
-                                <span className={`${className}`}>{word}</span>{" "}
+                                <span className={`${className}`}>{text}</span>{" "}
                               </React.Fragment>
                             ) : (
-                              <mark data-entity={`${className}`}>{word}</mark>
+                              <mark data-entity={`${className}`}>{text}</mark>
                             )
                           }
                           </>
                         );
-                      })
-                    }
-                    <hr />
-                    </div>
+                        })
+                      }
+                      <hr />
+                      </div>
                   ))}
-                <br />
+                  <br />
                 </div>
               );
             })
